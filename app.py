aa09615536dee556219af561ed58ced6d2f3bf2a
Taneli Junttila <taneli.junttila@edu.lapinamk.fi>
2025-02-26 14:15:02 +0200

    post endpoint

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello! World!"
+
+@app.route('/sentiment', methods=['POST'])
+def get_sentiment():
+    input_data = request.json
+    print(input_data)
+    # here be sentiment analysis
+    return {'input_data': input_data, 'message': 'hello!'}
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="8080", debug=False)
