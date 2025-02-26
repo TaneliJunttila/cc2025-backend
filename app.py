71757577587bbd4bb4e7eb114bbbbcdac4b6236d
Taneli Junttila <taneli.junttila@edu.lapinamk.fi>
2025-02-26 14:57:46 +0200

    Test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello World! Test4</h1>"
+    return "<h1>Hello World! Test5</h1>"
 
 @app.route('/sentiment', methods=['POST'])
 def get_sentiment():
